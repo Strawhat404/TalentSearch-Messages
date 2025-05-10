8e065307a2b34c1b1d29bc1a17b33d6ccfefe46b
kiya3300 <abelyitages10@gmail.com>
2025-05-10 22:39:14 +0300

    Remove jobs and userprofile directories

--- jobs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import JobListCreateView, JobDetailView
-
-urlpatterns = [
-    path('', JobListCreateView.as_view(), name='job-list-create'),
-    path('<int:job_id>/', JobDetailView.as_view(), name='job-detail'),
-]
