b7b527cc3ade12da0df09bd3e2a2aaad4d16e79d
kiya3300 <abelyitages10@gmail.com>
2025-05-10 11:38:52 +0300

    Fix build script to ensure gunicorn is installed

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# Exit on error
+set -e
+
+# Install dependencies
 pip install -r requirements/prod.txt
+pip install gunicorn
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
 python manage.py createsuperuser_if_not_exists
 python manage.py setup_roles
