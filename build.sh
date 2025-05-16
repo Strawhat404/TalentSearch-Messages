2de7fdb43b4a87c49e2de64514259bfe9d57b65a
kiya3300 <abelyitages10@gmail.com>
2025-05-16 10:37:29 +0300

    Add Render deployment configuration

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Python dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
+
 python manage.py createsuperuser_if_not_exists
 python manage.py setup_roles
