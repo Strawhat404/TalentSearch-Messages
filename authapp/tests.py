0ad78359954c2fd146d24f52203721aa8d4b3689
kiya3300 <abelyitages10@gmail.com>
2025-05-25 16:17:18 +0300

    fix: ensure LoginView always returns a response

--- authapp/tests.py
+++ authapp/tests.py
@@ -854,8 +854,8 @@ class PasswordResetTest(TestCase):
                 'uid': uid,
                 'token': token,
                 'new_password': 'newpass123'
-            })
-            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_multiple_reset_requests(self):
         """Test multiple reset requests invalidate previous tokens"""
@@ -924,8 +924,8 @@ class PasswordResetTest(TestCase):
                 'uid': uid,
                 'token': token,
                 'new_password': 'newpass123'
-            })
-            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 class AuthenticationEdgeCasesTest(TestCase):
     def setUp(self):
