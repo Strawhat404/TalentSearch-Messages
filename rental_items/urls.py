19f94ac77ce9933abd7d7aa1e9e9115b7eb9c7a1
kiya3300 <abelyitages10@gmail.com>
2025-06-30 22:05:03 +0300

    Fix ProfileSerializer profession field error

--- rental_items/urls.py
+++ rental_items/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RentalItemViewSet
+from .views import RentalItemViewSet, RentalItemListCreateView
 from rental_ratings.views import RatingViewSet
 
 router = DefaultRouter()
@@ -9,4 +9,5 @@ router.register(r'ratings', RatingViewSet, basename='rating')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('rental/', RentalItemListCreateView.as_view(), name='rentalitem-list-create'),
 ] 
