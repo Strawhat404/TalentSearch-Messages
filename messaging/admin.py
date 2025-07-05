648970af8693910d6f467541d69625263e7d78eb
strawhat404 <yosephtesfaye27@gmail.com>
2025-07-05 16:11:54 +0300

    Add messaging admin interface and API documentation

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, MessageThread
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
@@ -13,3 +13,24 @@ class MessageAdmin(admin.ModelAdmin):
     def message_preview(self, obj):
         return obj.message[:50] + ('...' if len(obj.message) > 50 else '')
     message_preview.short_description = 'Message'
+
+@admin.register(MessageThread)
+class MessageThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'participant_count', 'last_message_preview', 'created_at', 'updated_at', 'is_active')
+    list_filter = ('is_active', 'created_at', 'updated_at')
+    search_fields = ('title',)
+    ordering = ('-updated_at',)
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
+    filter_horizontal = ('participants',)
+
+    def participant_count(self, obj):
+        return obj.participants.count()
+    participant_count.short_description = 'Participants'
+
+    def last_message_preview(self, obj):
+        last_message = obj.get_last_message()
+        if last_message:
+            return last_message.message[:50] + ('...' if len(last_message.message) > 50 else '')
+        return 'No messages'
+    last_message_preview.short_description = 'Last Message'
